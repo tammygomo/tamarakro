cbdbc1edce7568ac47bf9ba8fa2d0998a3b9d4c5
Tamarakro Gomeromo <tamcomng@gmail.com>
2013-09-02 00:56:17 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<title> Tamarakro Gomeromo Official Website </title>
+		<link type="text/css" rel="stylesheet" href="css/styles.css" />
+	</head>
+	
+	<body>
+		<div style="background: url('imagas/line.png'); height: 2px;">
+			.
+		</div>
+		
+		<div id="header">
+			<p>
+				<img src="imagas/logo.png" />			
+			</p>
+		</div>
+			
+			
+		<div id="main">
+		<h2>
+			Hi there!
+		</h2>
+		
+		<p>
+			My name is Tamarakro Gomeromo, but fondly called Tammy <br \> by friends and family. I'm from Delta State, a southern region of <br \> Nigeria. Welcome to my personal and professional <br \> website.
+		</p>
+		
+		<p>
+			I studied Electrical and Electronics Engineering from  <a href="http://www.pti.edu.ng/"> Petroleum <br \> Training Institute</a>, Nigeria. I'm currently based in the City of <br \> Bangalore, India.
+		</p>
+		
+		<p>
+			Earlier this year, precisely February, I founded a company, <a href="http://www.connexxe.com"> Connexxe</a>, <br \> whose mission is to connect all international students spread across the <br \> globe.
+		</p>
+		
+		<p>
+			I'm a professional Web Developer, Android Mobile <br \> Application Developer and traditional Desktop <br \> Application Developer.
+		</p>
+		
+		<p>
+			I'm passionately in love with programming. Over the <br \> years, I have acquired numerous computer skills. Feel <br \> free to visit the "Resume/CV" page below. 
+		</p>
+		
+		</div>
+		<div id="footer">
+		<p>
+			 <a href="#" style="color: #085a80"> HOME</a> &nbsp; &nbsp; <a href="#" style="color: #ef1100"> RESUME/CV</a> &nbsp; &nbsp; <a href="#" style="color: #009122"> PHOTOS</a> &nbsp; &nbsp; <a href="#" style="color: #e96e0a"> BLOG</a> &nbsp; &nbsp; <a href="#" style="color: #8c0774"> CONTACT ME</a>
+
+		</p>
+		
+			<p> &copy; 2013, Tamarakro Gomeromo </p>
+		</div>
+		
+		
+	</body>
+</html>
